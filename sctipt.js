1fcdee49c15778418d9be5ac1a0478467c65c380
Lahcen <akdiml309@gmail.com>
2025-11-17 23:14:50 +0100

    Le regex (valider les informations) est disponible maintenant

--- sctipt.js
+++ sctipt.js
@@ -1,14 +1,18 @@
 //Les variaiables / tableaux //
-let Update_worker_btn = document.getElementById("Update-worker-btn");
-let close_btn = document.getElementById("close_btn");
+const Update_worker_btn = document.getElementById("Update-worker-btn");
+const close_btn = document.getElementById("close_btn");
 let counter = 0;
 let workers = [];
-let workercarte = document.querySelector(".workercarte");
+let allworkers = [];
+const workercarte = document.querySelector(".workercarte");
+const emailregex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{3,}$/gm ;
+const phoneregex = /^\d{10}$/;
+const lienregex = /^(https?:\/\/)?([a-zA-Z0-9-]+\.)+[a-zA-Z]{2,}(\/.*)?$/;
 
 
 //Les button//
 function displayform(){
-document.getElementById("blurbackround").style.display="flex";
+    document.getElementById("blurbackround").style.display="flex";
 }
 // submit //
 Update_worker_btn.addEventListener("click",(e)=>{
@@ -17,16 +21,35 @@ Update_worker_btn.addEventListener("click",(e)=>{
     let name = document.getElementById("name").value;
     let role = document.getElementsByTagName("select")[0].value;
     let photolink = document.getElementById("photolink");
-
-
-let email = document.getElementById("email").value;
-let phonenumber = document.getElementById("phonenumber").value;
+    
+    
+    let email = document.getElementById("email").value;
+    let phonenumber = document.getElementById("phonenumber").value;
+    // Regex //
 if(name==""||photolink.value==""||email==""||phonenumber==""){
-    alert("error");
-    e.preventDefault();
+        alert("error");
+        e.preventDefault();
     document.getElementById("blurbackround").style.display="flex";
 }
+    else if(!lienregex.test(photolink.value)){
+    alert("Le lien est pas correct")
+    document.getElementById("blurbackround").style.display="flex";
+    return;
+}
+    else if(!emailregex.test(email)){
+    alert("pardon , l'email est incorrect , entrez la form correct de gmail pour valider")
+    document.getElementById("blurbackround").style.display="flex";
+    return;
+}
+    else if(!phoneregex.test(phonenumber)){
+    alert("pardon , le numero est incorrect , entrez 10 nombres pour valider")
+    document.getElementById("blurbackround").style.display="flex";
+    return;
+}
+
+
 else{
+    // l'object principale 
     let worker = {
         id : counter ,
         workername : name ,
@@ -41,18 +64,29 @@ else{
     console.log(workers)
     console.log(worker.id)
     //Feauture add//
-workerszone.innerHTML+=`<div class="workercarte">
-                        <div class="workerimage"; style="background:url(${worker.workerphotolink});background-size:cover"></div>
-                        <div>
-                        <div><b>${worker.workername}</b></div>
-                       <div><p>${worker.workerrole}</p></div>
-                       </div>
-                       </div>`
-for(element of workerszone){
-workercarte.classList.add("workerszone");
-}
+    workerszone.innerHTML+=`<div class="workercarte" onClick ="listen(event)">
+    <div class="workerimage"; style="background:url(${worker.workerphotolink});background-size:cover"></div>
+    <div>
+    <div><b>${worker.workername}</b></div>
+    <div><p>${worker.workerrole}</p></div>
+    </div>
+    </div>`
+ 
+    let workerscartes = document.querySelectorAll(".workercarte");
+                    
+// for(element of workerscartes){
+// workercarte.classList.add("workerszone");
+// allworkers = querySelectorAll(".workercarte");
+// console.log(allworkers);
+// }
 }
 })
+function listen(e){
+    e.target.addEventListener("click",(v)=>{
+        alert("oui")
+    })
+ 
+}
 photolink.addEventListener("change",(e)=>{
     document.getElementById("imageplace").style.backgroundImage=`url(${photolink.value})`;
 })
