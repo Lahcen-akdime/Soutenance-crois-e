f1c328d3055e60a5674dd788321f0d2b2326ceca
Lahcen <akdiml309@gmail.com>
2025-11-20 16:15:27 +0100

    j'amileorer le locale storage , maintenant les donnés reste aprés d'actualiser la page web

--- sctipt.js
+++ sctipt.js
@@ -1,15 +1,12 @@
-//===================== Les variaiables / tableaux ==================================//
-let counter = 0;
-let counter2 = 0;
-let workers = [];
-let worker = {};
-let experiences ;
-let experience ={} ;
-let workerszone = document.getElementsByClassName("workerszone")[0];
-let workerexperiences = [];
-const Update_worker_btn = document.getElementById("Update-worker-btn");
-const close_btn = document.getElementById("close_btn");
-const workercarte = document.querySelector(".workercarte");
+//======================== Les variaiables / tableaux ==================================//
+let counter2 = 0 ; //pour le counter d'experience
+let workerszone = document.getElementsByClassName("workerszone")[0]; // aside worker place
+// ============================== Local storage data ===================================//
+let newid = JSON.parse(localStorage.getItem("id")) || 0 ;           // id de chaque object
+let workersmemory = JSON.parse(localStorage.getItem("worker"))||[];    // array of objects
+// =====================================================================================//
+const formCreateWorker = document.getElementById("form-create-worker");
+const workercarte = document.querySelector(".workercarte");          // class css de carte
 // regex
 const emailregex = /^[\w\-\.]+@([\w-]+\.)+[\w-]{3,}$/gm ;
 const phoneregex = /^\d{10}$/;
@@ -24,21 +21,16 @@ const workernumberinmodal = document.getElementById("worker-phone");
 const workerexperiencesplace = document.getElementById("experiences-place");
 // modal3 - add in section
 const selectmodal = document.getElementsByClassName("forblurmodal3")[0];
-
-// selectmodal.style.display="flex";
-
-
 // addexperieces
 const Addexperiencebtn = document.getElementById("Addexperience-btn");
 const toaddnewexperiencecarte = document.getElementById("allexperiences");
 const Experiencecarte = document.getElementsByClassName("Experiencecarte")[0];
-modaldescription.style.display="none";
 // =============================== Les button =========================================//
 function displayform(){
     document.getElementById("blurbackround").style.display="flex";
 }
 // =============================== submit ============================================ //
-Update_worker_btn.addEventListener("click",(e)=>{
+formCreateWorker.addEventListener("submit",(e)=>{
     e.preventDefault();
     document.getElementById("blurbackround").style.display="none";
     // ============================= inputs values =================================== //
@@ -52,8 +44,8 @@ Update_worker_btn.addEventListener("click",(e)=>{
         alert("error");
         e.preventDefault();
         document.getElementById("blurbackround").style.display="flex";
-}
-else if(!lienregex.test(photolink.value)){
+    }
+    else if(!lienregex.test(photolink.value)){
     alert("Le lien est pas correct")
     document.getElementById("blurbackround").style.display="flex";
     return;
@@ -68,10 +60,8 @@ else if(!phoneregex.test(phonenumber)){
     document.getElementById("blurbackround").style.display="flex";
     return;
 }
-
 else{
     // =========================== l'object d'experiences ================================= //
-    // inputs experiences values 
     let AllExperiencecartes = document.querySelectorAll(".Experiencecarte");
     let allexperiencestable = [];
     AllExperiencecartes.forEach(element => {
@@ -86,12 +76,12 @@ else{
             date_debut_with_company : datedebut,
             date_end_with_company : datefin,
         }
-        allexperiencestable.push(experience)
         counter2 +=1;
+        allexperiencestable.push(experience)
     });
     // ============================== l'object principale ================================= //
-    worker = {
-        id : counter ,
+    const worker = {
+        id : newid++ ,
         workername : name ,
         workerrole : role,
         workerphotolink : photolink.value,
@@ -99,25 +89,31 @@ else{
         workerphonenumber : phonenumber,
         experiences : allexperiencestable ,
     }
-    counter+=1;
-    workerszone = document.getElementsByClassName("workerszone")[0];
-    workers.push(worker)
-    workerexperiences.push(experiences);
-    console.log(worker)
+    workersmemory.push(worker)
     // ============================= Local storage setItem ==================================== //
-    localStorage.setItem("worker",JSON.stringify(workers));
-    // ===================================================================================== //
-    affichage(workerszone,worker.id,worker.workerphotolink,worker.workername,worker.workerrole);
-
-
-    // let workerscartes = document.querySelectorAll(".workercarte");
+    localStorage.setItem("worker",JSON.stringify(workersmemory));
+    localStorage.setItem("id",JSON.stringify(newid));
+    // =============================== Affichage ============================================== //
+    affichage();
     // ============================== reste the form ====================================== //
     document.getElementsByTagName("form")[0].reset();
+    workerexperiencesplace.innerHTML=`
+<div class="experiencediv">
+    <div>
+        <h4 style="display: inline;">Place : </h4>
+        <p style="display: inline;">${element.companyname}</p>
+    </div>
+            <ul>
+                <li><b>Role : </b><span>${element.role_in_company}</span></li>
+                <li><b>Period : </b><span>${element.date_debut_with_company}</span> / 
+                <span>${element.date_end_with_company}</span></li>
+            </ul>
+</div>`
 }
 })
     // ================================== display image ==================================== //
 photolink.addEventListener("change",(e)=>{
-    document.getElementById("imageplace").style.backgroundImage=`url(${photolink.value})`;
+document.getElementById("imageplace").style.backgroundImage=`url(${photolink.value})`;
 })
     // ================================= add a experience =================================== // 
 Addexperiencebtn.addEventListener("click",(e)=>{
@@ -134,58 +130,85 @@ toaddnewexperiencecarte.innerHTML+=`<div class="Experiencecarte">
 })
 // ============================= display modal of description =============================== //
 function showdescriptionmodal(e,objectid){
-    console.log(e.currentTarget)
+console.log(e.currentTarget)
+// let array1 = 
 modaldescription.style.display="flex";
-console.log(workers.find((x)=>x.id===objectid).workername);
-workernameinmodal.innerHTML=`${workers.find((x)=>x.id===objectid).workername}`
-workerroleinmodal.innerHTML=`${workers.find((x)=>x.id===objectid).workerrole}`
-workerimageinmodal.style.backgroundImage=`url(${workers.find((x)=>x.id===objectid).workerphotolink})`
-workeremailinmodal.innerHTML=`${workers.find((x)=>x.id===objectid).workeremail}`
-workernumberinmodal.innerHTML=`${workers.find((x)=>x.id===objectid).workerphonenumber}`
-let array = workers.find((x)=>x.id===objectid).experiences ;
-for(element of array) {
+let selectedworker = workersmemory.find((x)=>x.id===objectid);
+workernameinmodal.innerHTML=selectedworker.workername
+workerroleinmodal.innerHTML=selectedworker.workerrole
+workerimageinmodal.style.backgroundImage=`url(${selectedworker.workerphotolink})`
+workeremailinmodal.innerHTML=selectedworker.workeremail
+workernumberinmodal.innerHTML= selectedworker.workerphonenumber
+let array2 = selectedworker.experiences ;
+workerexperiencesplace.innerHTML="";
+for(element of array2) {
 workerexperiencesplace.innerHTML+=`
 <div class="experiencediv">
-    <div><h4 style="display: inline;">Place : </h4><p style="display: inline;">${element.companyname}</p></div>
-                <ul>
-                    <li><b>Role : </b><span>${element.role_in_company}</span></li>
-                    <li><b>Period : </b><span>${element.date_debut_with_company}</span>- <span>${element.date_end_with_company}</span></li>
-                </ul>
+    <div>
+        <h4 style="display: inline;">Place : </h4>
+        <p style="display: inline;">${element.companyname}</p>
+    </div>
+            <ul>
+                <li><b>Role : </b><span>${element.role_in_company}</span></li>
+                <li><b>Period : </b><span>${element.date_debut_with_company}</span> / 
+                <span>${element.date_end_with_company}</span></li>
+            </ul>
 </div>`
 };
-
 }
 function returntomain(){
     document.getElementById("blurbackround").style.display="none";
     modaldescription.style.display="none";
+selectmodal.style.display="none";
 }
 // ============================= Affichage ==================================== //
-function affichage(workerszone,workerid,workerphotolink,workername,workerrole){
-workerszone.innerHTML+=`<div class="workercarte" onClick ="showdescriptionmodal(event,${workerid})">
-    <div class="workerimage"; style="background:url(${workerphotolink});background-size:cover"></div>
-    <div>
-    <div><b>${workername}</b></div>
-    <div><p>${workerrole}</p></div>
-    </div>
-    </div>`
-    localStorage.setItem("div",workerszone.innerHTML);
+function affichage (){
+workerszone.innerHTML=""
+ workersmemory.forEach(element => {
+    workerszone.innerHTML+=`<div class="workercarte"
+     onClick="showdescriptionmodal(event,${element.id})">
+        <div class="workerimage"; style="background:url(${element.workerphotolink});background-size:cover"></div>
+        <div>
+        <div><b>${element.workername}</b></div>
+        <div><p>${element.workerrole}</p></div>
+        </div>
+        </div>`
+});
+}
+affichage()
+// ============================ Modal 3 ====================================== //
+selectmodal.style.display="flex";
+function showselectmodal(){
+workersmemory.forEach(element=>{
+
+
+
+
+
+    
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 }
-// let my_arr =[] ;
-//     if(JSON.parse(localStorage.getItem('worker'))!=null){
-//         my_arr = JSON.parse(localStorage.getItem('worker'));
-//         for(let i =0;i<my_arr.length;i++){
-//             workerszone.innerHTML+=`<div class="workercarte" onClick ="showdescriptionmodal(event,${Number(my_arr[i].id)})">
-//     <div class="workerimage"; style="background:url(${my_arr[i].workerphotolink});background-size:cover"></div>
-//     <div>
-//     <div><b>${my_arr[i].workername}</b></div>
-//     <div><p>${my_arr[i].workerrole}</p></div>
-//     </div>
-//     </div>`
-//         }
-//     }
-//  if(localStorage.getItem('div')!=null){
-//     workerszone.innerHTML= `${localStorage.getItem('div')}` 
-//  }
-   
+
+
+// =========================================================================== //
+
     
